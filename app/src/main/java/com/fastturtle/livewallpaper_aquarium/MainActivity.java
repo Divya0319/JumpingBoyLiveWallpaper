756d37572ce854f26d0ec05746dd324dc874c838
Divya Gupta <divygupta0319@gmail.com>
2022-11-27 10:50:02 +0530

    Updated .gitignore file, upgraded gradle version, and dependency versions

--- app/src/main/java/com/fastturtle/livewallpaper_aquarium/MainActivity.java
+++ app/src/main/java/com/fastturtle/livewallpaper_aquarium/MainActivity.java
@@ -4,10 +4,9 @@ import android.app.WallpaperManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     AppCompatButton btSetWall;
